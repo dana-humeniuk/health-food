8eeaedfdf8aeb9118f8f54a83140a18a9d0c23a3
Dana Humeniuk <dasyka08@gmail.com>
2025-09-14 18:13:02 +0300

    feat: enhance homepage with AOS and Typed.js integration
    - Added AOS (Animate On Scroll) for smooth animations on scroll events.
    - Integrated Typed.js for dynamic text effects in the hero section.
    - Updated package dependencies to include aos and typed.js.
    - Modified HTML structure to support new animations and dynamic text.
    - Enhanced styles for gradient text and button responsiveness.
    - Improved layout and accessibility features across various sections.

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,15 +1,32 @@
 import "../styles/main.scss";
 
-const initUIComponents = (): void => {};
+import Typed from "typed.js";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 document.addEventListener("DOMContentLoaded", (): void => {
-  initUIComponents();
+  AOS.init(
+    {
+      duration: 900,
+      easing: "ease-out-cubic",
+      once: true,
+      offset: 200,
+    }
+  );
 
-   //@ts-expect-error swiper declaration file is not found
-   const dishesSwiper = new Swiper(".dishes__main", {
+  const typed = new Typed("#typed-title", {
+    strings: ["live a healthier life", "eat well, feel great", "boost your energy"],
+    typeSpeed: 50,
+    loop: true,
+    backSpeed: 50,
+    showCursor: true,
+    cursorChar: "|",
+  });
+
+  //@ts-expect-error swiper declaration file is not found
+  const dishesSwiper = new Swiper(".dishes__main", {
     spaceBetween: 30,
 
-   
     breakpoints: {
       320: {
         slidesPerView: 1.3,
@@ -23,22 +40,25 @@ document.addEventListener("DOMContentLoaded", (): void => {
       1199: {
         slidesPerView: 3,
       },
-     
     },
   });
 
   //@ts-expect-error swiper declaration file is not found
   const reviewsSwiper = new Swiper(".reviews__main", {
+    navigation: {
+      nextEl: ".reviews__main-navigation-arrow--next",
+      prevEl: ".reviews__main-navigation-arrow--prev",
+    },
     spaceBetween: 30,
     breakpoints: {
-     320: {
+      320: {
         slidesPerView: 1.2,
       },
       480: {
         slidesPerView: 1.3,
       },
 
-     991: {
+      991: {
         slidesPerView: 2,
       },
     },
